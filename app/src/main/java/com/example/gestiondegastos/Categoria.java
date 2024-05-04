ad4c76d60f6c8d0f67aeeac9325c1c799015532b
Micaela Soto <micaela.soto@davinci.edu.ar>
2024-05-04 01:08:31 -0300

    MS - Agrega clase Categoría

---
+++ app/src/main/java/com/example/gestiondegastos/Categoria.java
@@ -0,0 +1,37 @@
+package com.example.gestiondegastos;
+
+public class Categoria {
+    private String nombre;
+    private String tipo;
+    private double monto;
+
+    private String fecha;
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo=tipo;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setMonto(double monto){
+        this.monto=monto;
+    }
+
+    public double getMonto(){
+        return monto;
+    }
+
+    public void setFecha(String fecha){
+        this.fecha=fecha;
+    }
+}
