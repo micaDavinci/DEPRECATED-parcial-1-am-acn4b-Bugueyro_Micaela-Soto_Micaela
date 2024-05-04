8494256a10580df43be3083e94bf9c3e7773b9a2
Bubu3110 <128099526+Bubu3110@users.noreply.github.com>
2024-05-03 22:50:20 -0300

    Se agrega el proyecto

---
+++ app/src/main/java/com/example/gestiondegastos/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.gestiondegastos;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    private Button botonIngreso;
+    private Button botonGasto;
+
+   
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botonIngreso = (Button) findViewById(R.id.botonIngreso);
+        botonGasto = (Button) findViewById(R.id.botonGasto);
+
+        botonIngreso.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        botonGasto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+}
+
+
